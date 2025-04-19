3dad59d5d21fa9a7c90b09bb4e6e0369184e1347
승환 지 <horong0602.dev@gmail.com>
2025-04-19 20:41:16 +0900

    NameSpace 개념

---
+++ 4 - NameSpace/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Namespace.
+
+// 이름이 같은 클래스들을 분류할 때 사용.
+
+namespace Horong
+{
+    class portion
+    {
+    }
+}
+
+namespace JSH
+{
+    class portion
+    {
+    }
+}
+
+class portion
+{
+}
