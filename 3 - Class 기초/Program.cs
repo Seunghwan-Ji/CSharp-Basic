b1981363eb0013a96a027dc837e92cb0e7ee90e7
승환 지 <horong0602.dev@gmail.com>
2025-04-19 20:40:54 +0900

    클래스 기초

---
+++ 3 - Class 기초/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// 객체 지향 프로그래밍 문법.
+// 내가 표현하고 싶은 것을 클래스로 만들어서
+// 그 클래스로 객체를 만드는 프로그래밍.
+
+class Player
+{
+}
+
+class Monster
+{
+}
+
+class Inven
+{
+}
