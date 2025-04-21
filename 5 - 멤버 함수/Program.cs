6d9ab8819e720bd37923005a157554eb0c9e66dd
Seunghwan-Ji <horong0602.dev@gmail.com>
2025-04-22 00:52:32 +0900

    멤버 함수

---
+++ 5 - 멤버 함수/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// RPG 게임을 만든다. -> 프로젝트를 만든다.
+// 캐릭터가 있다. -> 클래스를 만든다.
+// 캐릭터는 공격력이 있다. -> 멤버 변수를 만든다.
+// 캐릭터는 공격력으로 때린다. -> 멤버 함수를 만든다.
+
+class Player
+{
+    int ATT;
+    int HP;
+
+    void Fight()
+    {
+    }
+
+    void SkillUse()
+    {
+    }
+
+    void Talk()
+    {
+    }
+}
