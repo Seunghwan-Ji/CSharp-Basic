850e1d11920c8328423eb5b64e37980c3199fae8
Seunghwan-Ji <horong0602.dev@gmail.com>
2025-04-23 01:28:53 +0900

    메모리 구조

---
+++ 10 - 메모리 구조/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Player
+{
+}
+
+class Program
+{
+    static void Main()
+    {
+        Player NewPlayer = new Player();
+    }
+}
+
+// 모든 프로그램의 각 코드는 메모리를 소모한다.
+// 예를들어 함수는 스택 메모리에 할당.
